b25480b9bb08f9447b6647c1cffa16cb7bcfa71b
Peyton <peytonsemail2000@gmail.com>
2021-09-19 15:47:45 -0500

    Created a TypeOut function

---
+++ Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NarratorSideProject
+{
+    class Game
+    {
+        public void Run()
+        {
+            TypeOutWords("Uh... Hello there stranger. Nice to meet you", 100);
+            
+        }
+
+        public void TypeOutWords(string sentence, int timeBetweenLetters)
+        {
+            char[] chars = sentence.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                Console.Write(chars[i]);
+                Thread.Sleep(timeBetweenLetters);
+            }
+        }
+    }
+}
